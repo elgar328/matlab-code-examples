2ce1d34769905939e06f1195a62cf63b6b92c1cb
elgar328 <93251045+elgar328@users.noreply.github.com>
2022-11-29 20:48:56 +0900

    Update ProgressBar.m

--- tools/ProgressBar/ProgressBar.m
+++ tools/ProgressBar/ProgressBar.m
@@ -3,7 +3,7 @@
 %     USAGE
 %     |   task_name = 'downloading..';
 %     |   terminal_width = 50;
-%     |   
+%     |
 %     |   PB = ProgressBar;
 %     |   % or PB = ProgressBar(task_name);
 %     |   % or PB = ProgressBar(task_name, terminal_width);
@@ -18,7 +18,7 @@
 %     |       PB.update(ii/10)
 %     |   end
 %     |   PB.terminate
-%     |   
+%     |
 %     |   PB = ProgressBar;
 %     |   for ii = 1:5
 %     |       pause(1)
@@ -53,9 +53,9 @@ classdef ProgressBar < handle
         task_name
     end
     properties (SetAccess = private, Hidden = true)
-        bar = 0;
+        bar_n = 0;
         int_percent = 0;
-        char_set = {' ','█',' ','|'}; % <------------------------ Character set
+        char_set = {' ','█',' ','|'}; % <-------------------- Character set
     end
     properties (SetAccess = immutable, Hidden = true)
         terminal_width
@@ -71,22 +71,21 @@ classdef ProgressBar < handle
             arguments
                 task_name (1,:) char {mustBeText} = '';
                 terminal_width (1,1) {mustBePositive,mustBeFinite, ...
-                    mustBeReal,mustBeInteger} = 75
+                                      mustBeReal,mustBeInteger} = 75
             end
 
             assert(terminal_width >= obj.minimal_terminal_width, ...
                 'Terminal width must be greater than or equal to 20!')
             obj.terminal_width = terminal_width;
 
-            assert(length(task_name) < obj.terminal_width - ...
-                obj.minimal_terminal_width, 'task name is too long!')
-            obj.task_name = task_name;
+            obj.task_name = check_task_name(task_name,obj.terminal_width, ...
+                                            obj.minimal_terminal_width);
             if isempty(task_name)
                 obj.char_set{1} = '';
             end
 
-            % obj.bar_max = obj.terminal_width - 7 - length(task_name);
-            fprintf(repmat(' ',[1,obj.terminal_width]))
+            print_bar(obj.terminal_width,obj.task_name,obj.char_set, ...
+                      obj.bar_max,obj.bar_n,obj.int_percent,true,false)
         end
         % ---------------------- Dependent property -----------------------
         function bar_max = get.bar_max(obj)
@@ -100,19 +99,12 @@ classdef ProgressBar < handle
                 ratio (1,1) double {mustBeNonnegative,mustBeFinite,mustBeReal}
             end
             assert(ratio <= 1, 'The input argument, ratio, cannot exceed 1!')
-            if obj.bar ~= floor(obj.bar_max*ratio) || ...
+            if obj.bar_n ~= floor(obj.bar_max*ratio) || ...
                     obj.int_percent ~= floor(ratio*100)
-                obj.bar = floor(obj.bar_max*ratio);
+                obj.bar_n = floor(obj.bar_max*ratio);
                 obj.int_percent = floor(ratio*100);
-                bar_text = [repmat(obj.char_set{2},[1,obj.bar]), ...
-                    repmat(obj.char_set{3},[1,obj.bar_max-obj.bar])];
-
-                fprintf([repmat('\b',[1,obj.terminal_width]),'%s%c%c%s%c%3d%%'], ...
-                    obj.task_name, obj.char_set{1}, obj.char_set{4}, bar_text, ...
-                    obj.char_set{4},obj.int_percent)
-                if ratio == 1
-                    fprintf('\n')
-                end
+                print_bar(obj.terminal_width,obj.task_name,obj.char_set, ...
+                          obj.bar_max,obj.bar_n,obj.int_percent,false,false)
             end
         end
         function update_task_name(obj,task_name)
@@ -120,9 +112,9 @@ classdef ProgressBar < handle
                 obj
                 task_name (1,:) char {mustBeText} = '';
             end
-            assert(length(task_name) < obj.terminal_width - ...
-                obj.minimal_terminal_width, 'task name is too long!')
-            obj.task_name = task_name;
+
+            obj.task_name = check_task_name(task_name,obj.terminal_width, ...
+                                            obj.minimal_terminal_width);
             if isempty(task_name)
                 obj.char_set{1} = '';
             else
@@ -131,14 +123,37 @@ classdef ProgressBar < handle
         end
         % --------------------------- Terminator --------------------------
         function terminate(obj)
-            bar_text = [repmat(obj.char_set{2},[1,obj.bar]), ...
-                repmat(obj.char_set{3},[1,obj.bar_max-obj.bar])];
-            bar_text(end-9:end) = 'TERMINATED';
-
-            fprintf(repmat('\b',[1,obj.terminal_width]))
-            fprintf(2,'%s%c%c%s%c%3d%%\n', ...
-                obj.task_name, obj.char_set{1}, obj.char_set{4}, ...
-                bar_text, obj.char_set{4},obj.int_percent)
+            print_bar(obj.terminal_width,obj.task_name,obj.char_set, ...
+                      obj.bar_max,obj.bar_n,obj.int_percent,false,true)
         end
     end
+end
+% ---------------------------- Local functions ----------------------------
+function print_bar(terminal_width,task_name,char_set,bar_max,bar_n, ...
+                   int_percent,is_initial,is_terminated)
+bar_text = [repmat(char_set{2},[1,bar_n]), ...
+            repmat(char_set{3},[1,bar_max-bar_n])];
+if is_initial
+    back_space = '';
+else
+    back_space = repmat('\b',1,terminal_width);
+end
+if is_terminated
+    bar_text(end-9:end) = 'TERMINATED';
+    fprintf(back_space)
+    fprintf(2,'%s%c%c%s%c%3d%%\n',task_name,char_set{1}, ...
+        char_set{4},bar_text,char_set{4},int_percent);
+else
+    fprintf([back_space,'%s%c%c%s%c%3d%%'], ...
+        task_name,char_set{1},char_set{4},bar_text,char_set{4},int_percent);
+    if int_percent == 100 || is_terminated
+        fprintf('\n')
+    end
+end
+end
+
+function task_name = check_task_name(task_name,terminal_width,minimal_terminal_width)
+assert(length(task_name) < terminal_width - minimal_terminal_width, ...
+       sprintf('Length of the task name must be less than %d!', ...
+       terminal_width - minimal_terminal_width))
 end
